af4e665aed102f821e380056014e420398b1fbea
ChlorophyllA <71711825+ChlorophyllA@users.noreply.github.com>
2026-04-13 10:51:51 +0800

    Time: 3 ms (86.08%), Space: 136.7 MB (10.48%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        if(nums.size()<2*k+1){
-            vector<int> ans(nums.size(),-1);
+        long l=nums.size();
+        if(l<2*k+1){
+            vector<int> ans(l,-1);
             return ans;
         }
         vector<int> ans={};
         int i=0,j=2*k+1,m;
-        long long tmp=0;
+        long tmp=0;
         for(m=0;m<k;m++){
             ans.push_back(-1);
             // tmp+=nums[m];
@@ -17,11 +18,11 @@ public:
         }
         ans.push_back(tmp/(2*k+1));
 
-        for(;j<nums.size();i++,j++){
+        for(;j<l;i++,j++){
             tmp=tmp-nums[i]+nums[j];
             ans.push_back(tmp/(2*k+1));
         }
-        for(m=nums.size()-k;m<nums.size();m++){
+        for(m=l-k;m<l;m++){
             ans.push_back(-1);
         }
         return(ans);
