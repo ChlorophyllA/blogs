4d6dd40827bc783c0996be287054a5e9f9a210fe
ChlorophyllA <71711825+ChlorophyllA@users.noreply.github.com>
2026-04-14 18:49:46 +0800

    Time: 74 ms (61.32%), Space: 71.3 MB (13.5%) - LeetHub

--- 2954-maximum-sum-of-almost-unique-subarray/2954-maximum-sum-of-almost-unique-subarray.cpp
+++ 2954-maximum-sum-of-almost-unique-subarray/2954-maximum-sum-of-almost-unique-subarray.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     long long maxSum(vector<int>& nums, int m, int k) {
         std::unordered_map<int,int> ma={};
-        int i=0,j=k;
-        long long tmp=0,ans=0;
+        int i=0,j=k,tmp=0;
+        long long sum=0,ans=0;
         // bool flag=true;
         for(int n=0;n<k;n++){
             // if(flag && ma.size()<m){
@@ -11,20 +11,26 @@ public:
             // }
             if(ma.find(nums[n])==ma.end())
                 ma.insert({nums[n],1});
-            else ma[nums[n]]+=1;
-            tmp+=nums[n];
+            else{
+                if(ma[nums[n]]) tmp++;
+                ma[nums[n]]+=1;
+            }
+            sum+=nums[n];
         }
-        if(ma.size()>=m) ans=tmp;
+        if(k-tmp>=m) ans=sum;
         for(;j<nums.size();i++,j++){
             ma[nums[i]]-=1;
-            if(!ma[nums[i]]) ma.erase(nums[i]);
-            tmp=tmp-nums[i]+nums[j];
+            if(ma[nums[i]]) tmp--;
+            sum=sum-nums[i]+nums[j];
             if(ma.find(nums[j])==ma.end())
                 ma.insert({nums[j],1});
-            else ma[nums[j]]+=1;
-            if(ma.size()>=m)
-                ans=max(ans,tmp);
-            // cout<<tmp<<" i:"<<i<<" j:"<<j<<endl;
+            else{
+                if(ma[nums[j]]) tmp++;
+                ma[nums[j]]+=1;
+            }
+            if(k-tmp>=m)
+                ans=max(ans,sum);
+            // cout<<sum<<" i:"<<i<<" j:"<<j<<endl;
         }
         return(ans);
     }
