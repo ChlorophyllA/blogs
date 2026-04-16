d1a4f7a338a33fff82bea26a43d0bba1e85199cf
ChlorophyllA <71711825+ChlorophyllA@users.noreply.github.com>
2026-04-16 22:41:20 +0800

    Time: 692 ms (6.53%), Space: 45.3 MB (64.39%) - LeetHub

---
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        long long tmp=0,sum=0,ans=0;
+        long left=0,l=cardPoints.size(),right=l-k;
+        for(long i=0;i<l;i++){
+            sum+=cardPoints[i];
+        }
+        for(long j=0;j<l-k;j++){
+            tmp+=cardPoints[j];
+            cout<<tmp<<endl;
+        }
+        ans=sum-tmp;
+        for(;right<l;left++,right++){
+            tmp-=cardPoints[left];
+            tmp+=cardPoints[right];
+            cout<<tmp<<endl;
+            ans=max(ans,sum-tmp);
+        }
+        return(ans);
+    }
+};
