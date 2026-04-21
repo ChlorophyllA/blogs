c173057a8367475d71e8ddb3c54486dca79bc10f
ChlorophyllA <71711825+ChlorophyllA@users.noreply.github.com>
2026-04-21 16:52:25 +0800

    Time: 63 ms (16.88%), Space: 86.7 MB (5.44%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        long ans=0;
+        unordered_map<long,long> mmap={};
+        for (long left=0,right=0,tmp=0,l=0;right<fruits.size();right++){
+            l++;
+            if(mmap.contains(fruits[right])){
+                if(!mmap[fruits[right]]){
+                    tmp++;
+                }
+                mmap[fruits[right]]++;
+            }
+            else{
+                mmap.insert({fruits[right],1});
+                tmp++;
+            }
+            while(tmp>2){
+                mmap[fruits[left]]--;
+                if(!mmap[fruits[left]]){
+                    tmp--;
+                }
+                l--;
+                left++;
+            }
+            // if(tmp>)
+            ans=max(l,ans);
+        }
+        return ans;
+    }
+};
