fe5c10d5e22cdba216a26d96d5617879afc87908
ChlorophyllA <71711825+ChlorophyllA@users.noreply.github.com>
2026-04-21 16:56:41 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unsigned int ans=0;
-        unordered_map<unsigned int,unsigned int> mmap={};
-        for (unsigned int left=0,right=0,tmp=0,l=0;right<fruits.size();right++){
+        int ans=0;
+        unordered_map<int,int> mmap={};
+        for (int left=0,right=0,tmp=0,l=0;right<fruits.size();right++){
             l++;
             if(mmap.contains(fruits[right])){
                 if(!mmap[fruits[right]]){
