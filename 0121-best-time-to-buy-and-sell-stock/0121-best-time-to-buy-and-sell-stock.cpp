6502100f5b4b89a5952c6349bde88045fb3a92a4
ChlorophyllA <71711825+ChlorophyllA@users.noreply.github.com>
2026-05-06 12:24:23 +0800

    Time: 0 ms (100%), Space: 95.4 MB (20.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans=0,minn=0;
+        int ans=0,minn=prices[0];
         for(int i=1;i<prices.size();i++){
-            minn=prices[i]<prices[minn]?i:minn;
-            ans=max(ans,prices[i]-prices[minn]);
+            minn=prices[i]<minn?prices[i]:minn;
+            ans=max(ans,prices[i]-minn);
         }
         return ans;
     }
